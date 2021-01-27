235ce07556b33f765cb834f8063e3b16679c2e43
AmerigoM <amerigo.mancino@gmail.com>
2021-01-27 13:56:26 +0100

    Menu View Controller setup.

--- Online Flagship Store/Controller/ViewController.swift
+++ Online Flagship Store/Controller/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var readyToWear: UIButton!
+    @IBOutlet weak var accessories: UIButton!
+    @IBOutlet weak var lingerie: UIButton!
+    @IBOutlet weak var beauty: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        readyToWear.imageView?.contentMode = .scaleAspectFill
+        readyToWear.imageView?.layer.cornerRadius = 5
+        accessories.imageView?.contentMode = .scaleAspectFill
+        accessories.imageView?.layer.cornerRadius = 5
+        lingerie.imageView?.contentMode = .scaleAspectFill
+        lingerie.imageView?.layer.cornerRadius = 5
+        beauty.imageView?.contentMode = .scaleAspectFill
+        beauty.imageView?.layer.cornerRadius = 5
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("item")
+        print(sender.tag)
+    }
 }
 
