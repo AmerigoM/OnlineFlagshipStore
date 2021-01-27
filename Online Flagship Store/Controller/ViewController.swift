c39bd5950dc88744a32a2ab090ebc87e14ad4407
AmerigoM <amerigo.mancino@gmail.com>
2021-01-27 14:45:36 +0100

    Networking part setup for products list.

--- Online Flagship Store/Controller/ViewController.swift
+++ Online Flagship Store/Controller/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    // outlets
     @IBOutlet weak var readyToWear: UIButton!
     @IBOutlet weak var accessories: UIButton!
     @IBOutlet weak var lingerie: UIButton!
     @IBOutlet weak var beauty: UIButton!
+    
+    private var productList = [Product]()
+    
+    // constants
+    let READY_TO_WEAR_URL = "http://api.yoox.biz/Search.API/1.3/SMC_IT/search/results.json?ave=prod&productsPerPage=50&gender=D&page=1&department=Main_Ready_To_Wear&format=lite&sortRule=Ranking"
+    let ACCESSORIES_URL = "http://api.yoox.biz/Search.API/1.3/SMC_IT/search/results.json?ave=prod&productsPerPage=50&gender=D&page=1&department=Main_Accessories_All&format=lite&sortRule=Ranking"
+    let LINGERIE_URL = "http://api.yoox.biz/Search.API/1.3/SMC_IT/search/results.json?ave=prod&productsPerPage=50&page=1&department=Main_Lingerie&format=lite&sortRule=Ranking"
+    let BEAUTY_URL = "http://api.yoox.biz/Search.API/1.3/SMC_IT/search/results.json?ave=prod&productsPerPage=50&gender=D&page=1&department=Main_Beauty&format=lite&sortRule=Ranking"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +38,71 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-        print("item")
-        print(sender.tag)
+        switch sender.tag {
+        case 0:
+            getProductList(url: READY_TO_WEAR_URL)
+            break
+        case 1:
+            getProductList(url: ACCESSORIES_URL)
+            break
+        case 2:
+            getProductList(url: LINGERIE_URL)
+            break
+        case 3:
+            getProductList(url: BEAUTY_URL)
+            break
+        default:
+            self.displayAlert(title: "Unknown tag", message: "Please contact the app support.")
+        }
+    }
+    
+    // MARK: - Networking
+    
+    func getProductList(url: String) {
+        Alamofire.request(url, method: .get).responseJSON {
+            response in
+            if response.result.isSuccess {
+                let resultJSON: JSON = JSON(response.result.value!)
+                let productJSON = resultJSON["ResultsLite"]["Items"]
+                let jsonArray = productJSON.array!
+                
+                for item in jsonArray {
+                    let product = Product()
+                    product.title = item["ModelNames"].stringValue
+                    product.category = item["MicroCategory"].stringValue
+                    product.price = item["FullPrice"].stringValue
+                    product.thumbnailUrl = self.calculateThumbnailUrl(item["DefaultCode10"].stringValue)
+                    self.productList.append(product)
+                }
+            } else {
+                self.displayAlert(title: "Request failure", message: "Please contact the app support.")
+            }
+        }
+    }
+    
+    // MARK: - Support functions
+    
+    func calculateThumbnailUrl(_ defaultCode10: String) -> String {
+        var urlToReturn = "https://www.stellamccartney.com/"
+        
+        let folderIdentifier = String(defaultCode10.prefix(2))
+        urlToReturn.append(folderIdentifier)
+        urlToReturn.append("/")
+        urlToReturn.append(defaultCode10)
+        urlToReturn.append("_12_c.jpg")
+        
+        return urlToReturn
+    }
+    
+    func displayAlert(title: String, message: String) {
+        // Create new alert
+        let dialogMessage = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // Create OK button without action handler
+        let ok = UIAlertAction(title: "OK", style: .default)
+        
+        dialogMessage.addAction(ok)
+        self.present(dialogMessage, animated: true, completion: nil)
     }
 }
 
---
+++ Online Flagship Store/Model/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  Online Flagship Store
+//
+//  Created by Amerigo Mancino on 27/01/21.
+//
+
+import Foundation
+
+class Product {
+    var title: String = ""
+    var category: String = ""
+    var price: String = ""
+    var thumbnailUrl: String = ""
+}
